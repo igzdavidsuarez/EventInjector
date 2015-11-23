870d5541514c56bd4d699b62395828ccb5baab01
David Suarez <david.suarez@intelygenz.com>
2015-11-23 15:12:11 +0100

    Mysql Added

--- controllers/event/event.js
+++ controllers/event/event.js
@@ -1,8 +1,25 @@
 var express = require('express');
+var mysql = require('mysql');
 var router = express.Router();
 
-router.get('/test', function(req, res) {
-    res.send('Testing')
+router.get('/', function(req, res) {
+
+    var connection = mysql.createConnection({
+        host     : 'localhost',
+        user     : 'root',
+        password : 'root',
+        database : 'BDS_Poblacion_Banca'
+    });
+
+    connection.connect();
+
+    connection.query('SELECT * FROM ACTOR', function(err, rows, fields) {
+        if (err) throw err;
+
+        res.send(rows);
+    });
+
+    connection.end();
 });
 
 module.exports = router;
