e826d0762542ebf06103cb8279aba39b113a1560
David Suarez <david.suarez@intelygenz.com>
2015-11-23 13:46:18 +0100

    First Structure

--- bin/index.js
+++ bin/index.js
@@ -5,5 +5,5 @@ app.use(express.static(__dirname + '/public'));
 app.use(require('../controllers/router'));
 
 app.listen(3000, function() {
-    console.log('Listening on port 3000...')
+    console.log('Listening on port 3000...');
 });
--- controllers/router.js
+++ controllers/router.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-router.use('/test', require('./controller1/controller1'));
+router.use('/test', require('./event/event'));
 
 module.exports = router;
