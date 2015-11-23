e4f51c1b7bee46ee65234509001fa7838ab48b03
David Suarez <david.suarez@intelygenz.com>
2015-11-23 13:37:47 +0100

    Router Added

--- bin/index.js
+++ bin/index.js
@@ -2,8 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
-app.use(require('./middlewares'));
-app.use(require('./controllers'));
+app.use(require('../controllers/router'));
 
 app.listen(3000, function() {
     console.log('Listening on port 3000...')
--- controllers/router.js
+++ controllers/router.js
@@ -1,4 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-router.use('/test', require('./controller1'));
+router.use('/test', require('./controller1/controller1'));
+
+module.exports = router;
